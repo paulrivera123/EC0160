733925a27aa7f4b99a13055b43b839f2cbbdc08b
Paul.RiveraBarrera <paul.riverabarrera@cesunbc.edu.mx>
2025-10-19 20:41:05 -0700

    Almost done

--- EC0160/Form1.Designer.cs
+++ EC0160/Form1.Designer.cs
@@ -41,128 +41,268 @@
             this.txtFax = new System.Windows.Forms.TextBox();
             this.btnInsertar = new System.Windows.Forms.Button();
             this.btnConsultar = new System.Windows.Forms.Button();
-            this.btnActualizar = new System.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtCustomerID
             // 
-            this.txtCustomerID.Location = new System.Drawing.Point(120, 25);
+            this.txtCustomerID.Location = new System.Drawing.Point(160, 31);
+            this.txtCustomerID.Margin = new System.Windows.Forms.Padding(4);
             this.txtCustomerID.Name = "txtCustomerID";
-            this.txtCustomerID.Size = new System.Drawing.Size(150, 20);
+            this.txtCustomerID.Size = new System.Drawing.Size(199, 22);
             this.txtCustomerID.TabIndex = 0;
             // 
             // txtCompanyName
             // 
-            this.txtCompanyName.Location = new System.Drawing.Point(120, 50);
+            this.txtCompanyName.Location = new System.Drawing.Point(160, 62);
+            this.txtCompanyName.Margin = new System.Windows.Forms.Padding(4);
             this.txtCompanyName.Name = "txtCompanyName";
-            this.txtCompanyName.Size = new System.Drawing.Size(150, 20);
+            this.txtCompanyName.Size = new System.Drawing.Size(199, 22);
             this.txtCompanyName.TabIndex = 1;
             // 
             // txtContactName
             // 
-            this.txtContactName.Location = new System.Drawing.Point(120, 75);
+            this.txtContactName.Location = new System.Drawing.Point(160, 92);
+            this.txtContactName.Margin = new System.Windows.Forms.Padding(4);
             this.txtContactName.Name = "txtContactName";
-            this.txtContactName.Size = new System.Drawing.Size(150, 20);
+            this.txtContactName.Size = new System.Drawing.Size(199, 22);
             this.txtContactName.TabIndex = 2;
             // 
             // txtContactTitle
             // 
-            this.txtContactTitle.Location = new System.Drawing.Point(120, 100);
+            this.txtContactTitle.Location = new System.Drawing.Point(160, 123);
+            this.txtContactTitle.Margin = new System.Windows.Forms.Padding(4);
             this.txtContactTitle.Name = "txtContactTitle";
-            this.txtContactTitle.Size = new System.Drawing.Size(150, 20);
+            this.txtContactTitle.Size = new System.Drawing.Size(199, 22);
             this.txtContactTitle.TabIndex = 3;
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(120, 125);
+            this.txtAddress.Location = new System.Drawing.Point(160, 154);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(150, 20);
+            this.txtAddress.Size = new System.Drawing.Size(199, 22);
             this.txtAddress.TabIndex = 4;
             // 
             // txtCity
             // 
-            this.txtCity.Location = new System.Drawing.Point(120, 150);
+            this.txtCity.Location = new System.Drawing.Point(160, 185);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(4);
             this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(150, 20);
+            this.txtCity.Size = new System.Drawing.Size(199, 22);
             this.txtCity.TabIndex = 5;
             // 
             // txtRegion
             // 
-            this.txtRegion.Location = new System.Drawing.Point(120, 175);
+            this.txtRegion.Location = new System.Drawing.Point(160, 215);
+            this.txtRegion.Margin = new System.Windows.Forms.Padding(4);
             this.txtRegion.Name = "txtRegion";
-            this.txtRegion.Size = new System.Drawing.Size(150, 20);
+            this.txtRegion.Size = new System.Drawing.Size(199, 22);
             this.txtRegion.TabIndex = 6;
             // 
             // txtPostalCode
             // 
-            this.txtPostalCode.Location = new System.Drawing.Point(120, 200);
+            this.txtPostalCode.Location = new System.Drawing.Point(160, 246);
+            this.txtPostalCode.Margin = new System.Windows.Forms.Padding(4);
             this.txtPostalCode.Name = "txtPostalCode";
-            this.txtPostalCode.Size = new System.Drawing.Size(150, 20);
+            this.txtPostalCode.Size = new System.Drawing.Size(199, 22);
             this.txtPostalCode.TabIndex = 7;
             // 
             // txtCountry
             // 
-            this.txtCountry.Location = new System.Drawing.Point(120, 225);
+            this.txtCountry.Location = new System.Drawing.Point(160, 277);
+            this.txtCountry.Margin = new System.Windows.Forms.Padding(4);
             this.txtCountry.Name = "txtCountry";
-            this.txtCountry.Size = new System.Drawing.Size(150, 20);
+            this.txtCountry.Size = new System.Drawing.Size(199, 22);
             this.txtCountry.TabIndex = 8;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(120, 250);
+            this.txtPhone.Location = new System.Drawing.Point(160, 308);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(150, 20);
+            this.txtPhone.Size = new System.Drawing.Size(199, 22);
             this.txtPhone.TabIndex = 9;
             // 
             // txtFax
             // 
-            this.txtFax.Location = new System.Drawing.Point(120, 275);
+            this.txtFax.Location = new System.Drawing.Point(160, 338);
+            this.txtFax.Margin = new System.Windows.Forms.Padding(4);
             this.txtFax.Name = "txtFax";
-            this.txtFax.Size = new System.Drawing.Size(150, 20);
+            this.txtFax.Size = new System.Drawing.Size(199, 22);
             this.txtFax.TabIndex = 10;
             // 
             // btnInsertar
             // 
-            this.btnInsertar.Location = new System.Drawing.Point(12, 320);
+            this.btnInsertar.Location = new System.Drawing.Point(16, 394);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(4);
             this.btnInsertar.Name = "btnInsertar";
-            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.Size = new System.Drawing.Size(100, 28);
             this.btnInsertar.TabIndex = 11;
             this.btnInsertar.Text = "Insertar";
             this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(93, 320);
+            this.btnConsultar.Location = new System.Drawing.Point(124, 394);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.Size = new System.Drawing.Size(100, 28);
             this.btnConsultar.TabIndex = 12;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // btnActualizar
             // 
-            this.btnActualizar.Location = new System.Drawing.Point(174, 320);
+            this.btnActualizar.Location = new System.Drawing.Point(232, 394);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(4);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.Size = new System.Drawing.Size(100, 28);
             this.btnActualizar.TabIndex = 13;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(255, 320);
+            this.btnEliminar.Location = new System.Drawing.Point(340, 394);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.Size = new System.Drawing.Size(100, 28);
             this.btnEliminar.TabIndex = 14;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ID Cliente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Compañia:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "TItulo Contacto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Nombre Contacto:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Dirección:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Ciudad:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(72, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 16);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Región:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 252);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 16);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Código Postal:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(72, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 16);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "País:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(72, 314);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 16);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Teléfono:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(72, 344);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(32, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Fax:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 360);
+            this.ClientSize = new System.Drawing.Size(913, 443);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnConsultar);
@@ -178,10 +318,12 @@
             this.Controls.Add(this.txtContactName);
             this.Controls.Add(this.txtCompanyName);
             this.Controls.Add(this.txtCustomerID);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Gestión de Clientes";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -202,5 +344,16 @@
         private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Button btnActualizar;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
---
+++ Pruebasunitarias/DataClientetests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Datacliente;
+using System;
+
+// [TestClass] - Indica que esta clase contiene métodos de prueba
+[TestClass]
+public class DataClienteTests
+{
+    private DataCliente _dataCliente;
+
+    // [TestInitialize] - Se ejecuta antes de cada método de prueba
+    [TestInitialize]
+    public void Setup()
+    {
+        // Inicializamos la clase de acceso a datos para cada prueba
+        _dataCliente = new DataCliente();
+    }
+
+    // --- PRUEBA 1: Verificar la Inserción de un Cliente Válido (CRUD - Create) ---
+
+    // [TestMethod] - Indica que este es un método de prueba ejecutable
+    [TestMethod]
+    public void InsertarCustomer_DebeInsertarClienteValido_YRetornarTrue()
+    {
+        // ARRANGE (Preparación): Crear una entidad válida
+        string testId = "TEST1"; // Usa un ID que sabes que no existe
+
+        var clienteValido = new Customer
+        {
+            CustomerID = testId,
+            CompanyName = "Compañia Test",
+            ContactName = "Tester",
+            // Los demás campos son opcionales
+        };
+
+        // ACT (Acción): Llamar al método a probar
+        bool resultado = _dataCliente.insertarCustomer(clienteValido);
+
+        // ASSERT (Verificación): Confirmar que la inserción fue exitosa
+        Assert.IsTrue(resultado, "La inserción del cliente válido falló inesperadamente.");
+
+        // LIMPIEZA: Eliminar el registro para que la prueba pueda repetirse
+        _dataCliente.eliminarCustomer(testId);
+    }
+}
