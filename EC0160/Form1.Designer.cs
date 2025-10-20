72ef0d28929f0ca2fdb1d19a8e0a0e27fcb94907
Paul.RiveraBarrera <paul.riverabarrera@cesunbc.edu.mx>
2025-10-19 17:43:21 -0700

    Versión con multiples errores

--- EC0160/Form1.Designer.cs
+++ EC0160/Form1.Designer.cs
@@ -28,21 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtContactTitle = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtFax = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(397, 59);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(100, 22);
+            this.txtCustomerID.TabIndex = 0;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(397, 104);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(100, 22);
+            this.txtCompanyName.TabIndex = 1;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(397, 175);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(100, 22);
+            this.txtContactName.TabIndex = 2;
+            // 
+            // txtContactTitle
+            // 
+            this.txtContactTitle.Location = new System.Drawing.Point(397, 230);
+            this.txtContactTitle.Name = "txtContactTitle";
+            this.txtContactTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtContactTitle.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(607, 62);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 22);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(607, 120);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 22);
+            this.txtCity.TabIndex = 5;
+            // 
+            // txtFax
+            // 
+            this.txtFax.Location = new System.Drawing.Point(827, 175);
+            this.txtFax.Name = "txtFax";
+            this.txtFax.Size = new System.Drawing.Size(100, 22);
+            this.txtFax.TabIndex = 10;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(827, 120);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone.TabIndex = 9;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(827, 65);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 22);
+            this.txtCountry.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ID cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nombre de Compañia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nombre de contacto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Titulo de Contacto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(521, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Dirección";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(535, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Ciudad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(541, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Región";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(503, 236);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Código postal";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(753, 71);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 16);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "País";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(753, 126);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 16);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Teléfono";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(753, 181);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 16);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Fax";
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(607, 175);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(100, 22);
+            this.txtRegion.TabIndex = 22;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(607, 236);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 22);
+            this.txtPostalCode.TabIndex = 23;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(304, 301);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 24;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(422, 301);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 25;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(544, 301);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 26;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(677, 301);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 27;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1228, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFax);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtContactTitle);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtCustomerID);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtContactTitle;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtFax;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtRegion;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
--- EC0160/Form1.cs
+++ EC0160/Form1.cs
@@ -1,25 +1,135 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Negociocliente; // Importamos el namespace de nuestra Capa de Negocio
 
-namespace EC0160
+namespace EC0160.Presentation
 {
+    // El modificador 'partial' es crucial para combinar este código con el diseño.
     public partial class Form1 : Form
     {
+        // Solución al error CS0118: Usamos el nombre de clase completo para evitar ambigüedad.
+        private Negociocliente.Negociocliente negocio = new Negociocliente.Negociocliente();
+
         public Form1()
         {
+            // Esta llamada es CRUCIAL para inicializar y "encontrar" los controles.
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // --- MÉTODOS AUXILIARES ---
+        private void MapControlsToNegocio()
         {
+            // Mapeo del formulario -> Negocio
+            negocio.CustomerID = txtCustomerID.Text;
+            negocio.CompanyName = txtCompanyName.Text;
+            negocio.ContactName = txtContactName.Text;
+            negocio.ContactTitle = txtContactTitle.Text;
+            negocio.Address = txtAddress.Text;
+            negocio.City = txtCity.Text;
+            negocio.Region = txtRegion.Text;
+            negocio.PostalCode = txtPostalCode.Text;
+            negocio.Country = txtCountry.Text;
+            negocio.Phone = txtPhone.Text;
+            negocio.Fax = txtFax.Text;
+        }
 
+        private void MapNegocioToControls()
+        {
+            // Mapeo del Negocio -> formulario (para la consulta)
+            txtCustomerID.Text = negocio.CustomerID;
+            txtCompanyName.Text = negocio.CompanyName;
+            txtContactName.Text = negocio.ContactName;
+            txtContactTitle.Text = negocio.ContactTitle;
+            txtAddress.Text = negocio.Address;
+            txtCity.Text = negocio.City;
+            txtRegion.Text = negocio.Region;
+            txtPostalCode.Text = negocio.PostalCode;
+            txtCountry.Text = negocio.Country;
+            txtPhone.Text = negocio.Phone;
+            txtFax.Text = negocio.Fax;
+        }
+
+        private void ClearControls()
+        {
+            // Limpia todas las TextBoxes
+            txtCustomerID.Clear();
+            txtCompanyName.Clear();
+            txtContactName.Clear();
+            txtContactTitle.Clear();
+            txtAddress.Clear();
+            txtCity.Clear();
+            txtRegion.Clear();
+            txtPostalCode.Clear();
+            txtCountry.Clear();
+            txtPhone.Clear();
+            txtFax.Clear();
+        }
+
+        // --- EVENTOS DE LOS BOTONES (CRUD) ---
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            MapControlsToNegocio();
+
+            if (string.IsNullOrEmpty(negocio.CustomerID))
+            {
+                MessageBox.Show("El ID del cliente no puede estar vacío.", "Error de Inserción", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (negocio.insertar())
+            {
+                MessageBox.Show("Cliente agregado.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearControls();
+            }
+            else
+            {
+                MessageBox.Show("Error: El cliente ya existe o faltan datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            negocio.CustomerID = txtCustomerID.Text;
+
+            if (negocio.cargar())
+            {
+                MapNegocioToControls();
+            }
+            else
+            {
+                MessageBox.Show("Cliente no encontrado.", "Error de Consulta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ClearControls();
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            MapControlsToNegocio();
+
+            if (negocio.actualizar())
+            {
+                MessageBox.Show("Cambio exitoso.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Error: El ID del cliente no existe.", "Error de Actualización", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            negocio.CustomerID = txtCustomerID.Text;
+
+            if (negocio.eliminar())
+            {
+                MessageBox.Show("Cliente borrado.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearControls();
+            }
+            else
+            {
+                MessageBox.Show("Error: ID inexistente o el cliente tiene órdenes asociadas.", "Error de Eliminación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
